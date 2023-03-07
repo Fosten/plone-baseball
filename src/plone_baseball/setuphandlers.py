b5601b8a319dc16b2aa3baff10a23d08d248deae
fosten <fosten@gmail.com>
2023-03-07 16:59:58 -0500

    lint-black code prettier

--- src/plone_baseball/setuphandlers.py
+++ src/plone_baseball/setuphandlers.py
@@ -6,6 +6,7 @@ from plone import api
 import requests
 import json
 
+
 @implementer(INonInstallable)
 class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
@@ -18,32 +19,47 @@ class HiddenProfiles(object):
         """Hide the upgrades package from site-creation and quickinstaller."""
         return ["plone_baseball.upgrades"]
 
+
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
     portal = api.portal.get()
     post_content(portal)
 
+
 def post_content(portal):
-    if not portal.get('import-players', False):
+    if not portal.get("import-players", False):
         mainfolder = api.content.create(
-            type='Document',
+            type="Document",
             container=portal,
-            title='Import Players',
-            id='import-players'
+            title="Import Players",
+            id="import-players",
         )
 
-    json_array = json.loads(requests.get('https://statsapi.mlb.com/api/v1/sports/1/players?season=2023').text)
-    store_list = [{'nameFirstLast': item['nameFirstLast'], 'nameSlug': item['nameSlug'], 'id': item['id']} for item in json_array['people']]
+    json_array = json.loads(
+        requests.get(
+            "https://statsapi.mlb.com/api/v1/sports/1/players?season=2023"
+        ).text
+    )
+    store_list = [
+        {
+            "nameFirstLast": item["nameFirstLast"],
+            "nameSlug": item["nameSlug"],
+            "id": item["id"],
+        }
+        for item in json_array["people"]
+    ]
 
     for item in store_list:
-          obj = api.content.create(
-            type='playercard',
-            title=item['nameFirstLast'],
-            id=item['nameSlug'],
-            playerID=item['id'],
-            container=portal['import-players'])
-        
+        obj = api.content.create(
+            type="playercard",
+            title=item["nameFirstLast"],
+            id=item["nameSlug"],
+            playerID=item["id"],
+            container=portal["import-players"],
+        )
+
+
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
