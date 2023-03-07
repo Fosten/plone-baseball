675b3935de4c9de278351ce6986e8a1d0c4863ec
fosten <fosten@gmail.com>
2023-03-07 16:49:51 -0500

    Import all players

--- src/plone_baseball/setuphandlers.py
+++ src/plone_baseball/setuphandlers.py
@@ -2,6 +2,9 @@
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
+from plone import api
+import requests
+import json
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
@@ -15,12 +18,32 @@ class HiddenProfiles(object):
         """Hide the upgrades package from site-creation and quickinstaller."""
         return ["plone_baseball.upgrades"]
 
-
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    portal = api.portal.get()
+    post_content(portal)
 
+def post_content(portal):
+    if not portal.get('import-players', False):
+        mainfolder = api.content.create(
+            type='Document',
+            container=portal,
+            title='Import Players',
+            id='import-players'
+        )
 
+    json_array = json.loads(requests.get('https://statsapi.mlb.com/api/v1/sports/1/players?season=2023').text)
+    store_list = [{'nameFirstLast': item['nameFirstLast'], 'nameSlug': item['nameSlug'], 'id': item['id']} for item in json_array['people']]
+
+    for item in store_list:
+          obj = api.content.create(
+            type='playercard',
+            title=item['nameFirstLast'],
+            id=item['nameSlug'],
+            playerID=item['id'],
+            container=portal['import-players'])
+        
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
